dc792c25e1c91ffb4bbc9640c4c447bfb0884bad
Mkups1 <m.kupilik1@share.epsb.ca>
2022-02-18 13:28:00 -0700

    thing

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 // User login JS
 
-let Username = document.getElementById("usrname")
+let username = document.getElementById("usrname");
+let password = document.getElementById("passwrd");
+// Event listener
+document.getElementById("login-btn").addEventListener("click",login);
+// Event function
+function login() {
+    // input
+    console.log(username.innerHTML)
+    if (username === "admin"){
+        alert("Login Successful")
+    }
+}
